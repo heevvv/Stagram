b05a10ef128fad9776ac3801e0b3ba26b4104c81
LAPTOP-6UTEKP1F\qwert <qwert38320@naver.com>
2023-02-07 20:05:21 +0900

    [신희경] Stagram 최초 파일 추가

---
+++ src/main/java/com/heekyung/stagram/StagramApplication.java
@@ -0,0 +1,13 @@
+package com.heekyung.stagram;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StagramApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StagramApplication.class, args);
+	}
+
+}
